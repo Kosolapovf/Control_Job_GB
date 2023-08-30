c8f3b93b1ba4a9c3cc5290653023ea7955bab9ff
Egor <EgorrKosolapoff@ya.ru>
2023-08-30 18:01:28 +0700

    create array method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
-﻿Console.Clear();
+﻿void InputArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("введите элементы для вывода: ");
+        array[i] = Console.ReadLine();
+    }
+    Console.Write($"[{string.Join(", ", array)}]");
+    Console.WriteLine("");
+}
+
+Console.Clear();
 Console.Write("введите колчитсво элементов в массиве: ");
 int num = Convert.ToInt32(Console.ReadLine());
 string[] array = new string[num];
+InputArray(array);
