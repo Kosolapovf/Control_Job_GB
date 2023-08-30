17611b92b94323f966db205c502689a0fe3dd994
Egor <EgorrKosolapoff@ya.ru>
2023-08-30 19:38:17 +0700

    create method of creating an array according to the condition

--- Program.cs
+++ Program.cs
@@ -9,8 +9,24 @@
     Console.WriteLine("");
 }
 
+void ReleaseArray(string[] array)
+{
+    string[] massiv = new string[array.Length];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            massiv[count] = array[i];
+            count++;
+        }
+    }
+    Console.Write($"[{string.Join(", ", massiv)}]");
+    
+}
 Console.Clear();
 Console.Write("введите колчитсво элементов в массиве: ");
 int num = Convert.ToInt32(Console.ReadLine());
 string[] array = new string[num];
 InputArray(array);
+ReleaseArray(array);
